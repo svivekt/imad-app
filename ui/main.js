a07d83fe3bb3be0382a7deaedc2d6362dc3cdefd
svivekt <30918919+svivekt@users.noreply.github.com>
2017-08-19 10:38:13 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerTML = 'NEW Value';
+
+var img = document.getElementById('madi');
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function () {
+    var internal = setInterval(moveRight, 50);
+};
